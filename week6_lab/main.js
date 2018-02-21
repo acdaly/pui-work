8a9a6677f593efc7ca4a6a9e2ff2577f5816194e
acdaly <acdaly@andrew.cmu.edu>
2018-02-21 15:09:19 -0500

    finished week6lab

--- week6_lab/main.js
+++ week6_lab/main.js
@@ -49,7 +49,19 @@ $(document).on("click", ".delete-item", function() {
 $(document).on("click", "#add-item", function() {
     var list = $("#grocery-list");
     var itemInput = $("#new-list-item");
-    list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>");
+    list.append("<li>" + "<input id = 'checkbox' type='checkbox'>" + itemInput.val() + 
+        " <button class='delete-item'>X</button></li>"
+        );
 });
 
+$(document).on("click", ".checkbox", function() {
+    if ($(this).is(":checked")){ 
+        console.log("yes!");
+        $(this).parent().css('text-decoration','line-through');
+    }
+    else{
+        $(this).parent().css('text-decoration','none');
+        }
+    
+});
 
