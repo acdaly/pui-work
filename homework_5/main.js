4d1e34dc42538f062ef70d3da476119513d428c9
acdaly <acdaly@andrew.cmu.edu>
2018-03-27 11:36:33 -0400

    can locally save add to cart data and added API and Bootstrap Demos from lab

--- homework_5/main.js
+++ homework_5/main.js
@@ -6,16 +6,14 @@ function Pillow(name, shape, color, quantity, price) {
     this.price = price;
 }
 
-var pillows = [];
+
+var selectedPillow;
 var selectedShape = "square";
 var selectedColor = "black";
-
-function savePillow() {
-
-}
+var selectedQuantity = 1;
+var pillows = [];
 
 function handleShapeClick(clickedID) {
-    console.log(clickedID);
     $('#'+selectedShape).css('background-color', 'white');
     $('#'+clickedID).css('background-color', 'lightgray');
     selectedShape = clickedID;
@@ -24,17 +22,48 @@ function handleShapeClick(clickedID) {
 }
 
 function handleColorClick(clickedID) {
-    
     $('#'+selectedColor).removeClass('outlineClass');
     $('#'+clickedID).addClass('outlineClass');
-    
-    console.log("clicked!");
-/*
-    $('#'+selectedColor).css('outline', 1);
-    $('#'+selectedColor).css('outline-color', 'lightgray');
-    $('#'+selectedColor).css('outline-offset', 1);
-    $('#'+clickedID).css('outline-color', 'black');
-    $('#'+clickedID).css('outline', '3px');
-    $('#'+clickedID).css('outline-offset', '3px');*/
     selectedColor = clickedID;
+    selectedColor = clickedID;
+}
+
+function handleQuantityClick(clickedID) {
+    selectedQuantity = parseInt($('#input-quantity').val());
+}
+
+function savePillow(newPillow) {
+    if (localStorage.getItem('savedPillows') == null) {
+        pillows = [];
+    }
+    else {
+        pillows = localStorage.getItem('savedPillows');
+    }
+    pillows.push(newPillow);
+    localStorage.setItem('savedPillows', JSON.stringify(pillows));
+}
+
+function handleAddToCart(){
+
+    var selectedPillow = "Bed Pillow";
+
+    if (selectedShape == "square") {
+        price = 85;
+    }
+    else if (selectedShape == "round") {
+        price = 85;
+    }
+    else if (selectedShape == "dog") {
+        price = 135;
+    }
+    else if (selectedShape == "cat") {
+        price = 135;
+    }
+    else if (selectedShape == "bunny") {
+        price = 165;
+    }
+    var newPillow = new Pillow(selectedPillow, selectedShape, selectedColor, 
+                               selectedQuantity, price);
+    savePillow(newPillow);
+    
 }
