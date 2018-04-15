405ccfbdadd4cd03908c753d6dc72a023c0e1a09
acdaly <acdaly@andrew.cmu.edu>
2018-04-15 17:39:49 -0400

    added connect, back button working, cleaned up main.js

--- final_project/Assets/Javascript/main.js
+++ final_project/Assets/Javascript/main.js
@@ -1,41 +1,85 @@
-$( document ).ready(function() {
-    console.log("ready");
+var featured = '<div id="Featured">\
+          <div class="row">\
+            <div class = "col-sm-12 spacer"></div>\
+          </div>\
+          <div class="row justify-content-center">\
+            <div id="featured-1" class = "col-md-5 portfolio-img">\
+              <h2 class="portfolio-title">Google Light Prototype</h2>\
+              <img id="1" src="Assets/images/featured/1.jpg">\
+            </div>\
+            <div class = "col-md-5 portfolio-img">\
+              <h2 class="portfolio-title">Sandblox</h2>\
+              <img src="Assets/images/featured/2.png">\
+            </div>\
+\
+            <div class = "col-md-5 portfolio-img">\
+              <h2 class="portfolio-title">LED Controller</h2>\
+              <img src="Assets/images/featured/3.jpg">\
+            </div>\
+            <div class = "col-md-5 portfolio-img">\
+              <h2 class="portfolio-title">Entanglement</h2>\
+              <img src="Assets/images/featured/4.jpg">\
+            </div>\
+            <div class = "col-md-5 portfolio-img">\
+              <h2 class="portfolio-title">Surround Sounds</h2>\
+              <img src="Assets/images/featured/5.jpg">\
+            </div>\
+            <div class = "col-md-5 portfolio-img">\
+              <h2 class="portfolio-title">Dream</h2>\
+              <img src="Assets/images/featured/6.jpg">\
+            </div>\
+          </div>\
+        </div>';
 
-    $('.carousel').carousel({
-      interval: 4000
-    });
-
-    $('#portfolio .nav-link' ).on( 'click', function () {
-    //references https://stackoverflow.com/questions/24514717/bootstrap-navbar-active-state-not-working
-    $('#portfolio .navbar').find( '.active' ).removeClass( 'active' );
-    $( this ).addClass( 'active' );
-    });
-
-    $(".portfolio-img img").hover(function(){
-    //references https://stackoverflow.com/questions/14616818/how-to-use-stop-properly-in-jquery-animation-with-hover-event
-        $(this).stop(true).fadeTo(200, 0);
+function onPortfolioImgHover(){   
+    $(".portfolio-img").on('mouseenter', 'img', function(event){
+        $(this).fadeTo(200, 0);
         $(this).parent().children('h2').fadeTo(200, 1);
-    },
-        function(){
-            $(this).stop(true).fadeTo(200, 1);
+    }).on('mouseleave', 'img', function(event){
+            $(this).fadeTo(200, 1);
             $(this).parent().children('h2').fadeTo(200, 0);
         });
+};
+
+function onPortfolioImgClick(){ 
     $(".portfolio-img").click(function(){
         console.log(this);
         $('#Featured').stop(true).fadeTo(500, 0,
             function(){
                 $('#Featured').remove();
                 $('#portfolio').append('\
-                    <div id="google-prototype">\
+                    <div class="portfolio-subsection">\
                       <div class= "back">\
                         < Back\
                       </div>\
                     </div>');
+                $('.portfolio-subsection').delegate('.back', 'click', function(){
+                    $(this).parent().remove();
+                    $('#portfolio').append(featured);
+                    onPortfolioImgHover(); 
+                    onPortfolioImgClick();
+                });
             });
-        
-
     });
-});
+
+}
+
+$( document ).ready(function() {
+    $('.carousel').carousel({
+      interval: 4000
+    });
+    $('#portfolio .nav-link' ).on( 'click', function () {
+    //references https://stackoverflow.com/questions/24514717/bootstrap-navbar-active-state-not-working
+    $('#portfolio .navbar').find( '.active' ).removeClass( 'active' );
+    $( this ).addClass( 'active' );
+    });
+    onPortfolioImgClick();
+    onPortfolioImgHover();
+
+
+    
+    
+ });
 
 
 
