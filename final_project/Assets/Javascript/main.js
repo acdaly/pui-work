63794996ea5e99c81b0881344da261eea45ce8f5
acdaly <acdaly@andrew.cmu.edu>
2018-04-22 12:21:32 -0400

    added p5 footer

--- final_project/Assets/Javascript/main.js
+++ final_project/Assets/Javascript/main.js
@@ -31,6 +31,59 @@ var featured = '<div id="Featured">\
           </div>\
         </div>';
 
+var googlePrototype = '\
+  <div id="google-detail">\
+    <div class="row">\
+      <div class = "col-sm-12 spacer"></div>\
+    </div>\
+    <div class="row justify-content-around align-items-center">\
+      <div id="google-carousel-container" class = "col-sm-5 spacer">\
+        <!-- Carousel\
+        copied from bootstrap documentation\
+          https://getbootstrap.com/docs/4.0/components/carousel/ -->\
+        <div id="google-carousel" class="carousel slide" data-ride="carousel">\
+          <ol class="carousel-indicators">\
+            <li data-target="#google-carousel" data-slide-to="0" class="active"></li>\
+            <li data-target="#google-carousel" data-slide-to="1"></li>\
+            <li data-target="#google-carousel" data-slide-to="2"></li>\
+          </ol>\
+          <div id="google-carousel-inner" class="carousel-inner">\
+            <div class="carousel-item active">\
+              <img class="d-block w-100" src="Assets/images/google-prototype/1.jpg" alt="First slide">\
+            </div>\
+            <div class="carousel-item">\
+              <img class="d-block w-100" src="Assets/images/google-prototype/2.png" alt="Second slide">\
+            </div>\
+            <div class="carousel-item">\
+              <img class="d-block w-100" src="Assets/images/google-prototype/3.jpg" alt="Third slide">\
+            </div>\
+          </div>\
+          <a class="carousel-control-prev" href="#google-carousel" role="button" data-slide="prev">\
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>\
+            <span class="sr-only">Previous</span>\
+          </a>\
+          <a class="carousel-control-next" href="#google-carousel" role="button" data-slide="next">\
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>\
+            <span class="sr-only">Next</span>\
+          </a>\
+        </div>\
+        <!-- End of Carousel -->\
+      </div>\
+      <div class = "col-sm-5">\
+        <p id="google-text">\
+          This prototype was made during an internship at Iontank, a specialized design studio that \
+          develops interactive art installations and software systems. The original design was created \
+          during the ideation stage for an installation with their client, Google, and is now a component of the in-progress project.<br>\
+          <br>\
+          The design was then iterated upon to create a standalone prototype.\
+          <br>\
+          <br>\
+        </p>\
+        <iframe src="https://player.vimeo.com/video/259328050" width="640" height="352" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>\
+      </div>\
+    </div>\
+  </div>';
+
 function onPortfolioImgHover(){   
     $(".portfolio-img").on('mouseenter', 'img', function(event){
         $(this).fadeTo(200, 0);
@@ -42,27 +95,29 @@ function onPortfolioImgHover(){
 };
 
 function onPortfolioImgClick(){ 
-    $(".portfolio-img").click(function(){
-        console.log(this.id);
-        $('#Featured').stop(true).fadeTo(500, 0,
-            function(){
-                $('#Featured').remove();
-                $('#portfolio').append('\
-                    <div class="portfolio-subsection">\
-                      <div class= "back">\
-                        < Back\
-                      </div>\
-                    </div>');
-                if (this.id == 'google-prototype'){
-                    
-                }
-                $('.portfolio-subsection').delegate('.back', 'click', function(){
-                    $(this).parent().remove();
-                    $('#portfolio').append(featured);
-                    onPortfolioImgHover(); 
-                    onPortfolioImgClick();
-                });
+  $(".portfolio-img").click(function(){
+    var loadID = this.id;
+    $('#Featured').stop(true).fadeTo(500, 0,
+        function(){
+            $('#Featured').remove();
+            $('#portfolio').append('\
+                <div class="portfolio-subsection">\
+                  <div class= "back">\
+                    < Back\
+                  </div>\
+                </div>');
+            if (loadID == 'google-prototype'){
+              console.log("loading google");
+              $('.portfolio-subsection').append(googlePrototype);
+                
+            }
+            $('.portfolio-subsection').delegate('.back', 'click', function(){
+                $(this).parent().remove();
+                $('#portfolio').append(featured);
+                onPortfolioImgHover(); 
+                onPortfolioImgClick();
             });
+        });
     });
 };
 
